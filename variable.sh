f9717cbeefe7c02fccc810c61a6736a364a0ca0e
Rupesh <gosi.rupesh@capgemini.com>
2025-12-30 22:54:56 +0530

    updated variables

--- variable.sh
+++ variable.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#variables
+#variablesgit
 PERSON1=Mokshitha
 PERSON2=Pooja
 
-echo "PERSON1 : hello how are you"
-echo "PERSON2 : fine and how about you"
-echo "PERSON1 : good and thank you"
-echo "PERSON2 : bye tc... will meet you soon"
+echo "$PERSON1 : hello how are you"
+echo "$ERSON2 : fine and how about you"
+echo "$PERSON1 : good and thank you"
+echo "$PERSON2 : bye tc... will meet you soon"
