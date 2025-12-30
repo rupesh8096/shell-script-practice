55f08b1e5ce8350fb9e53ee71f3d9a6542f20f7a
Rupesh <gosi.rupesh@capgemini.com>
2025-12-30 22:53:38 +0530

    variables

---
+++ variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#variables
+PERSON1=Mokshitha
+PERSON2=Pooja
+
+echo "PERSON1 : hello how are you"
+echo "PERSON2 : fine and how about you"
+echo "PERSON1 : good and thank you"
+echo "PERSON2 : bye tc... will meet you soon"
