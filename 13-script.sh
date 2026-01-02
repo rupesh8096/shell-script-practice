f904e1d12b9446b3035ba37ce9fadbdb7e3e7a07
Rupesh <gosi.rupesh@capgemini.com>
2026-01-02 21:53:16 +0530

    mysql1234

--- 13-script.sh
+++ 13-script.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo " This user in not root user"
+    exit 1
 fi
 
 dnf install  mysql -y
 
 if [ $? -ne 0 ]; then 
     echo "installed mysql as root user"
+    exit 1
 else
     echo " mysql not installed"
 fi
