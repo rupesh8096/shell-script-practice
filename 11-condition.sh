c959964bdc070cd7321df3750db35c9c9376b9a4
Rupesh <gosi.rupesh@capgemini.com>
2026-01-02 20:08:51 +0530

    ifcondition2

---
+++ 11-condition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter number:"
+read NUMBER
+
+if [$ (($NUMBER%2)) -eq to 0 ];then
+    echo "Given number $NUMBER is even"
+else
+    ehco "Given number $NUMBER is ODD"
+fi
