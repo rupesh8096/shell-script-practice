bfb134021a37931221dcae6f972096f51e7c5358
Rupesh <gosi.rupesh@capgemini.com>
2026-01-02 16:05:06 +0530

    updated5variable

--- 5-variable.sh
+++ 5-variable.sh
@@ -4,3 +4,8 @@ echo "Please enter your PIN number"
 read -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
+
+echo "Please eneter YOUR name"
+read -s YOUR_NAME 
+
+echo "My name is : $YOUR_NAME"
